2f5259e4444d569e009624139fc390a678802b81
Gwagsi <gwagsiglenn@gmail.com>
2023-01-09 21:02:01 +0100

    test

---
+++ src/components/Signup.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react'
+
+export default class Signup extends PureComponent {
+  render() {
+    return (
+      <div><form>
+      <label>
+        Name
+        <input type="text" name="name" />
+      </label>
+      <input type="text" value="text" />
+    </form></div>
+      
+    )
+  }
+}
